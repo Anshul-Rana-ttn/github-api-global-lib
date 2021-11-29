172b178e9fa79938a841da0d580f935125560514
Ansh <90328753+Anshul-Rana-ttn@users.noreply.github.com>
2021-11-29 10:34:54 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,4 @@
 def call(Map config = [:]) {
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+//     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+    sh "echo hello Anshul Rana Welcome to jenkins"
 }
